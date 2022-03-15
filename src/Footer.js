c803dddf0c72f40dac2001c412ec1c634d1bce1a
calizemi <cm.zegarra@gmail.com>
2022-03-14 23:02:19 -0500

    creando headder y footer

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <div>
+        <footer>
+          <h4>All right reserved @CopyRight</h4>
+        </footer>
+      </div>
+    );
+  };
+  
+  export default Footer;
