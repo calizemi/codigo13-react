e81afb7672532701c8e90a8594d95d09a10718a1
calizemi <cm.zegarra@gmail.com>
2022-03-14 21:41:40 -0500

    primer cambio en react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// El primer paso para definir un componente es el nombre
+// El nombre de un componente siempre debe iniciar en Mayuscula
+// El componente es una funcion
+// podemos crearlo usando function o arow function
+const PrimerComponente=()=>{
+      // Esta funcion retorna una vista
+      // en react usamos return()para indicar que lo que esta dentro de los parentesis
+      // sera el objeto visual que vamos a retornar
+      // algo importante es que react solo puede retornar un div
+      return(
+        <div>
+          <h1>Hola mundo</h1>
+          <div>
+            <h4>Hola codigo</h4>
+          </div>
+        </div>
+      );
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//Luego de crear el componente debemos exportarlo
 
-export default App;
+
+export default PrimerComponente;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import PrimerComponente from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* para importar un archivo debemos colocarlo como una etiqueta */}
+    <PrimerComponente />
   </React.StrictMode>,
   document.getElementById('root')
 );
